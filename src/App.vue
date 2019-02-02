1a8b5799751bce4db025dd3d433c6ac6c28617ec
Tikken <tikken23@gmail.com>
2019-02-02 23:25:28 +0300

    push

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Tikken</span>
+        <span class="font-weight-light">'s table</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Github</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <Table/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Table from "./components/Table.vue";
+
+export default {
+  name: "App",
+  components: {
+    Table
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <Table/>
+</template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  components: {
+    Table
+  }
+};
+</script>
